558007766eaf590bebbade86e511a959e1afe81b
Omid Taheri <omid.taheri@tuebingen.mpg.de>
2020-06-15 12:56:25 +0300

    small update

--- cvpr_2020_scheduler.py
+++ cvpr_2020_scheduler.py
@@ -1,8 +1,6 @@
-
-from configer import Configer
 from tools import search_program
 from tools import set_timezone
-
+from tools import dotdict
 
 
 if __name__ == '__main__':
@@ -10,18 +8,17 @@ if __name__ == '__main__':
 
     params = {
         'program': 'papers', # ['papers','tutorials','workshops']
-        'authors':None, #['john', 'Bob'], # ['john smith', 'Bob', 'Alex']
+        'authors':['john'], #['john', 'Bob'], # ['john smith', 'Bob', 'Alex']
         'keywords':['hand'], # [ '3D', 'VIBE', 'human object interaction' ]
-        'dates': None, # [19, 13]
-        'times': [(8,12)], #,[(start_time), (end_time)],
+        'dates': [15], # [19, 13]
+        'times': [(8,12)], #,[(start_time), (end_time)] in your time zone,
         'paper_id': None, #[7,2582], # [123, 2345]
 
-        'time_zone': None,
+        'time_zone': 'Europe/berlin',
         'export_ics': True,
     }
 
-    ps = Configer(default_ps_fname='default_params',**params)
+    ps = dotdict(params)
     ps.time_zone = set_timezone(ps.time_zone)
 
     search_program(ps)
-    print('finish')
